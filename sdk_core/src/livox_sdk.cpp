6a656d27fab051caa05fedc23a618232ec26f77b
David-Michael Buckman <dmbuck32@gmail.com>
2022-08-15 12:04:39 +1200

    add logging to sdk startup.

--- sdk_core/src/livox_sdk.cpp
+++ sdk_core/src/livox_sdk.cpp
@@ -21,6 +21,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
+#include <memory>
 
 #include "livox_sdk.h"
 #include "command_handler/command_handler.h"
@@ -28,104 +29,169 @@
 #include "base/logging.h"
 #include "device_manager.h"
 #ifdef WIN32
-#include<winsock2.h>
+#include <winsock2.h>
 #endif // WIN32
+
 
 using namespace livox;
-IOThread *g_thread = NULL;
+
+static std::unique_ptr<IOThread> g_thread = nullptr;
 static bool is_initialized = false;
 
-void GetLivoxSdkVersion(LivoxSdkVersion *version) {
-  if (version != NULL) {
-    version->major = LIVOX_SDK_MAJOR_VERSION;
-    version->minor = LIVOX_SDK_MINOR_VERSION;
-    version->patch = LIVOX_SDK_PATCH_VERSION;
-  }
+void GetLivoxSdkVersion(LivoxSdkVersion *version)
+{
+    if (version != NULL)
+    {
+        version->major = LIVOX_SDK_MAJOR_VERSION;
+        version->minor = LIVOX_SDK_MINOR_VERSION;
+        version->patch = LIVOX_SDK_PATCH_VERSION;
+    }
 }
 
-bool Init() {
-  if (is_initialized) {
-    return false;
-  }
+bool Init()
+{
+    if (is_initialized)
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Already initialized.");
+        return false;
+    }
+
 #ifdef WIN32
-  WORD sockVersion = MAKEWORD(2, 0);
-  WSADATA wsdata;
-  if (WSAStartup(sockVersion, &wsdata) != 0) {
-    return false;
-  }
+    WORD sockVersion = MAKEWORD(2, 0);
+    WSADATA wsdata;
+    if (WSAStartup(sockVersion, &wsdata) != 0)
+    {
+        return false;
+    }
 #endif // WIN32
 
-  bool result = false;
-  do {
+    is_initialized = false;
+
+    // Initialise the logging.
     InitLogger();
 
-    g_thread = new IOThread();
-    g_thread->Init();
-
-    if (device_discovery().Init() == false) {
-      result = false;
-      break;
+    if( nullptr != g_thread )
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Thread not null");
+        return false;
     }
 
-    if (device_manager().Init() == false) {
-      result = false;
-      break;
+    // Create the global IO thread.
+    g_thread = std::make_unique<IOThread>();
+    const bool thread_initialized = g_thread->Init();
+    if (!thread_initialized)
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Failed to initialize thread.");
+        return false;
     }
 
-    if (command_handler().Init(g_thread->loop()) == false) {
-      result = false;
-      break;
-    }
-    if (data_handler().Init() == false) {
-      result = false;
-      break;
+
+    // Initialize the device discovery.
+    const bool device_discovery_initialized = device_discovery().Init();
+    if (!device_discovery_initialized)
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Failed to initialize device discovery.");
+        return false;
     }
 
-    result = true;
-  } while (0);
+    // Initialize the device manager.
+    const bool device_manager_initialized = device_manager().Init();
+    if (!device_manager_initialized)
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Failed to initialize device manager.");
+        return false;
+    }
 
-  is_initialized = result;
-  return result;
+    // Initialize the command handler.
+    const bool command_handler_initialized = command_handler().Init(g_thread->loop());
+    if (!command_handler_initialized)
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Failed to initialize command handler.");
+        return false;
+    }
+
+    // Initialize the data handler.
+    const bool data_handler_initialized = data_handler().Init();
+    if (!data_handler_initialized)
+    {
+        LOG_ERROR("Failed to initialize Livox SDK - Failed to initialize data handler.");
+        return false;
+    }
+
+    is_initialized = true;
+    return true;
 }
 
-void Uninit() {
-  if (!is_initialized) {
-    return;
-  }
+void Uninit()
+{
+    if (!is_initialized)
+    {
+        LOG_ERROR("Failed to uninitialize Livox SDK - Not initialized.");
+        return;
+    }
+
 #ifdef WIN32
     WSACleanup();
 #endif // WIN32
-  if (g_thread) {
-    g_thread->Quit();
-    g_thread->Join();
-  }
 
-  if (g_thread) {
-    g_thread->Uninit();
-    delete g_thread;
-    g_thread = NULL;
-  }
+    device_discovery().Uninit();
 
-  device_discovery().Uninit();
-  command_handler().Uninit();
-  data_handler().Uninit();
-  device_manager().Uninit();
-  
-  UninitLogger();
-  is_initialized = false;
+    command_handler().Uninit();
+
+    data_handler().Uninit();
+
+    device_manager().Uninit();
+
+    if (nullptr != g_thread)
+    {
+        // Quit any running processes.
+        g_thread->Quit();
+
+        // Join any outstanding threads.
+        g_thread->Join();
+
+        // Uninitialize the thread.
+        g_thread->Uninit();
+    }
+
+    UninitLogger();
+
+    is_initialized = false;
 }
 
-bool Start() {
-  if (g_thread && device_discovery().Start(g_thread->loop()) && g_thread->Start()) {
+bool Start()
+{
+    const bool thread_available = (nullptr != g_thread);
+    if (!thread_available)
+    {
+        LOG_ERROR("Failed to start Livox SDK - thread not available.");
+        return false;
+    }
+
+    const bool device_discovery_started = device_discovery().Start(g_thread->loop());
+    if (!device_discovery_started)
+    {
+        LOG_ERROR("Failed to start Livox SDK - device discovery failed to start.");
+        return false;
+    }
+
+    const bool thread_started = g_thread->Start();
+    if (!thread_started)
+    {
+        LOG_ERROR("Failed to start Livox SDK - thread failed to start.");
+        return false;
+    }
+
+    // Livox SDK started successfully.
     return true;
-  }
-  return false;
 }
 
-void SaveLoggerFile() {
-  is_save_log_file = true;
+void SaveLoggerFile()
+{
+    is_save_log_file = true;
 }
 
-void DisableConsoleLogger() {
-  is_console_log_enable = false;
+void DisableConsoleLogger()
+{
+    is_console_log_enable = false;
 }
